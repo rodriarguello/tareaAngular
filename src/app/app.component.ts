f548e65e61d56c4398960dafaf7ac34f772daad4
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-07-13 20:14:50 -0300

    Desarrollo de la logica del componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from './models/producto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tareaAngular';
+  
+  constructor(){
+
+    this.mostrar = true;
+  }
+  
+  listProductos:Producto[] = [{
+    nombre:'Leche',
+    costo:'500'
+  },
+  {
+    nombre:'Azucar',
+    costo:'600'
+  },
+  {
+    nombre:'Aceite',
+    costo:'1000'
+  },
+  {
+    nombre:'Dulce de leche',
+    costo:'500'
+  },
+  {
+    nombre:'Manteca',
+    costo:'400'
+  }
+  ];
+
+  public mostrar:boolean;
+
+  modificarVisibilidad(){
+    this.mostrar = !this.mostrar;
+  }
+
+
+
 }
